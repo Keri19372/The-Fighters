e542fd478f2080bbdd731f38074eaf43fdf008c6
Dmytro Rudiachuk <keri19372@gmail.com>
2020-05-11 19:52:49 +0300

    finished game

---
+++ src/javascript/components/modal/winner.js
@@ -0,0 +1,11 @@
+import { showModal } from "./modal";
+
+export function showWinnerModal(fighter) {
+	// call showModal function 
+	const winnerInfo = {
+		title: '...And The WINNER!',
+		bodyElement: fighter.name
+	}
+
+	showModal(winnerInfo);
+}
